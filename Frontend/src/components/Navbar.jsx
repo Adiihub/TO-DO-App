61fc18ce1594e740dd7d9e4758954b2545eae86b
Aditi Kumari <aditi22202@gmail.com>
2024-10-05 15:27:18 +0530

    Frontend Complete

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,42 +1,41 @@
-import React, { useState } from 'react'
-import SearchBar from './SearchBar/SearchBar'
-import ProfileInfo from './Cards/ProfileInfo'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import SearchBar from "./SearchBar/SearchBar";
+import ProfileInfo from "./Cards/ProfileInfo";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-    const [SearchQuery, setSearchQuery] = useState("");
-    const navigate = useNavigate()
+  const [SearchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
 
-    const handleSearch = () => {
+  const handleSearch = () => {};
 
-    }
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
 
-    const onClearSearch = () => {
-        setSearchQuery("");
-    }
-
-    const onLogout = () => {
-        navigate("/login")  // yha navigate krenge through React-Router-DOM
-    }
+  const onLogout = () => {
+    navigate("/login"); // yha navigate krenge through React-Router-DOM
+  };
 
   return (
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-        <h2 className='text-xl font-medium text-black py-2'>
-            <span className='text-slate-500'>Good</span>
-            <span className='text-slate-900'>Notes</span>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <Link to={"/"}>
+        <h2 className="text-xl font-medium text-black py-2">
+          <span className="text-slate-500">Good</span>
+          <span className="text-slate-900">Notes</span>
         </h2>
+      </Link>
 
+      <SearchBar
+        value={SearchQuery}
+        onChange={({ target }) => setSearchQuery(target.value)}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
 
-        <SearchBar 
-        value={SearchQuery} 
-        onChange={({target}) => setSearchQuery(target.value)} 
-        handleSearch={handleSearch} 
-        onClearSearch={onClearSearch}/>
-
-        <ProfileInfo onLogout={onLogout}/>
-
+      <ProfileInfo onLogout={onLogout} />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
-import React from "react";
-import NoteCard from "../../components/Cards/NoteCard";
+import React, { useState } from "react";
 import { MdAdd } from "react-icons/md";
+import Modal from "react-modal";
+import AddEditNotes from "./AddEditNotes";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
+  const [openAddEditModel, setOpenAddEditModel] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
   return (
     <>
       <div className="container mx-auto ">
@@ -77,10 +84,32 @@ const Home = () => {
 
       <button
         className="w-16 h-16 flex items-center justify-center rounded-2xl bg-[#2B85FF] hover:bg-blue-600 absolute right-10 bottom-10"
-        onClick={() => {}}
+        onClick={() => {
+          setOpenAddEditModel({ isShown: true, type: "add", data: null });
+        }}
       >
         <MdAdd className="text-[32px] text-white" />
       </button>
+
+      <Modal
+        isOpen={openAddEditModel.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-md:w-[60%] max-sm:w-[70%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes
+          onclose={() =>
+            setOpenAddEditModel({ isShown: false, type: "add", data: null })
+          }
+          noteData={openAddEditModel.data}
+          type={openAddEditModel.type}
+        />
+      </Modal>
     </>
   );
 };
